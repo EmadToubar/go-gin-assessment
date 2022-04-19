6b65d66de10d0a2059d529e662aa25faaf90b26d
Emad-T <b00073720@aus.edu>
2022-04-19 11:04:09 +0400

    Minor fixes

--- service/appointmentService.go
+++ service/appointmentService.go
@@ -18,45 +18,50 @@ type appointmentService struct {
 }
 
 func (ap *appointmentService) AddAppointments(appoint models.Appointment) (*models.Appointment, error) {
-	result, err := ap.AddAppointments(appoint)
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
+	// result, err := ap.AddAppointments(appoint)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return result, nil
+	return ap.AddAppointments(appoint)
 
 }
 
 func (ap *appointmentService) GetAppointments() ([]models.Appointment, error) {
-	result, err := ap.GetAppointments()
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
+	// result, err := ap.GetAppointments()
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return result, nil
+	return ap.GetAppointments()
 }
 
 func (ap *appointmentService) GetAppointment(appointid int) (*models.Appointment, error) {
-	result, err := ap.GetAppointment(appointid)
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
+	// result, err := ap.GetAppointment(appointid)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return result, nil
+	return ap.GetAppointment(appointid)
 
 }
 
 func (ap *appointmentService) GetPatientHistory(patientid string) ([]models.Appointment, error) {
-	result, err := ap.GetPatientHistory(patientid)
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
+	// result, err := ap.GetPatientHistory(patientid)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return result, nil
+	return ap.GetPatientHistory(patientid)
 }
 
 func (ap *appointmentService) GetMaxAppointments() ([]models.CountResponse, error) {
-	result, err := ap.GetMaxAppointments()
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
+	// result, err := ap.GetMaxAppointments()
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return result, nil
+	return ap.GetMaxAppointments()
 }
 
 func AppointmentServiceProvider(db *datab.AppointmentDB) AppointmentService {
